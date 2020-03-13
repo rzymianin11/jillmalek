b0b896adcaea8beba60056611f7053ef03e481ef
Lukasz Drozdz <lukaszdrozdz@iMac-Lukasz.local>
2020-03-13 13:37:38 +0100

    processes

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -11,6 +11,10 @@ import '../../styles/theme.scss.liquid';
 import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
 import {cookiesEnabled} from '@shopify/theme-cart';
 
+import Process from '../sections/process';
+
+Process.init();
+
 // Common a11y fixes
 focusHash();
 bindInPageLinks();
---
+++ src/scripts/sections/process.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+const Process = {
+	init : function(){
+		  $('.slickjs').slick({
+
+			});
+	}
+}
+
+export default Process
