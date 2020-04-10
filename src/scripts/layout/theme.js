a95cfb33bf432c21d65b0161a0e5c45d5cd5b3f1
Piotr Osmola <piotr.osmola@gmail.com>
2020-04-11 01:54:28 +0200

    Update about carousels, footer, mobile horizontal views, prev next files;

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,76 +1,103 @@
-import 'lazysizes/plugins/object-fit/ls.object-fit';
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import 'lazysizes/plugins/rias/ls.rias';
-import 'lazysizes/plugins/bgset/ls.bgset';
-import 'lazysizes';
-import 'lazysizes/plugins/respimg/ls.respimg';
+import 'lazysizes/plugins/object-fit/ls.object-fit'
+import 'lazysizes/plugins/parent-fit/ls.parent-fit'
+import 'lazysizes/plugins/rias/ls.rias'
+import 'lazysizes/plugins/bgset/ls.bgset'
+import 'lazysizes'
+import 'lazysizes/plugins/respimg/ls.respimg'
 
-import '../../styles/theme.scss';
-import '../../styles/theme.scss.liquid';
+import '../../styles/theme.scss'
+import '../../styles/theme.scss.liquid'
 
-import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
-import {cookiesEnabled} from '@shopify/theme-cart';
+import { focusHash, bindInPageLinks } from '@shopify/theme-a11y'
+import { cookiesEnabled } from '@shopify/theme-cart'
 
-import Process from '../sections/process';
-import Shopwallpaper from '../sections/shop-wallpaper';
-import MobileHeader from '../sections/mobilenav';
+import Process from '../sections/process'
+import Shopwallpaper from '../sections/shop-wallpaper'
+import MobileHeader from '../sections/mobilenav'
 
-import Cart from './cart';
+import Cart from './cart'
 
-import $ from 'jquery';
+import $ from 'jquery'
 
-import './jquery.parallax.min.js';
+import './jquery.parallax.min.js'
+
+import imagesLoaded from 'imagesloaded'
+
+imagesLoaded.makeJQueryPlugin($)
+
+// Images Fade In
+$('#MainContent')
+  .imagesLoaded()
+  .progress((instance, image) => {
+    $(image.img).addClass('in')
+  })
+
+// Sidebar Filters
+$('.sidebar-filter-toggle').on('click', function() {
+  $(this)
+    .next('.sidebar-filter')
+    .addClass('visible')
+
+  return false
+})
+$('.sidebar-filter-close').on('click', function() {
+  $(this)
+    .parents('.sidebar-filter')
+    .removeClass('visible')
+
+  return false
+})
 
 $('.my-parallax-window').parallax({
-  speed: -.2,
+  speed: -0.2,
   sliderSelector: '>.my-parallax-slider',
   mirrorSelector: '.parallax-mirror',
   zIndex: 10,
-  excludeAgents: false,
+  excludeAgents: false
 })
 
-Process.init();
-Shopwallpaper.init();
-MobileHeader.init();
-Cart.init();
+Process.init()
+Shopwallpaper.init()
+MobileHeader.init()
+Cart.init()
 
 // Common a11y fixes
-focusHash();
-bindInPageLinks();
+focusHash()
+bindInPageLinks()
 
 // Apply a specific class to the html element for browser support of cookies.
 if (cookiesEnabled()) {
-  document.documentElement.className = document.documentElement.className.replace(
-    'supports-no-cookies',
-    'supports-cookies',
-  );
+  document.documentElement.className = document.documentElement.className.replace('supports-no-cookies', 'supports-cookies')
 }
 
-$(function() {
-    //caches a jQuery object containing the header element
-/*
-    var header = $("#shopify-section-header");
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
+$(() => {
+  // caches a jQuery object containing the header element
+  /*
+        var header = $("#shopify-section-header");
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
 
-        if (scroll >= 100) {
-            header.addClass("small");
-        } else {
-            header.removeClass('small');
-        }
-    });
-    
-*/
-	
-	$('a[href^="#"]').click(function (e) {
-		console.log('animate');
-		e.preventDefault();
-		e.stopPropagation();
-		var el = $($(this).attr('scrollto')).offset().top - 150;
-	    $('html, body').animate({
-		    scrollTop: el
-		}, 500);
-	
-	    return false;
-	});
-});
+            if (scroll >= 100) {
+                header.addClass("small");
+            } else {
+                header.removeClass('small');
+            }
+        });
+
+    */
+
+  $('a[href^="#"]').click(function(e) {
+    console.log('animate')
+    e.preventDefault()
+    e.stopPropagation()
+    const el = $($(this).attr('scrollto')).offset().top - 150
+    $('html, body').animate(
+      {
+        scrollTop: el
+      },
+      500
+    )
+
+    return false
+  })
+})
--- src/scripts/sections/process.js
+++ src/scripts/sections/process.js
@@ -1,201 +1,201 @@
-import $ from 'jquery';
-import 'slick-carousel';
-import "slick-carousel/slick/slick.css";
+import $ from 'jquery'
+import 'slick-carousel'
+import 'slick-carousel/slick/slick.css'
 
-var jQuery = $;
+const jQuery = $
+;(function($) {
+  // create temporal object to get slick object
 
-(function($) {
-  'use strict';
+  // !!!!!!!!!!!
+  // getSlick() -> TO CHYBA CZASEM ROZWALA CAY SKRYPT (Maximum stack exceded)
+  // !!!!!!!!!!!
 
-  //create temporal object to get slick object
-  var getSlick = function() {
-    var $tmp = $('<div>').slick();
-    var slick = $tmp[0].slick.constructor;
-    $tmp.slick('unslick');
-    return slick;
-  };
+  const getSlick = function() {
+    const $tmp = $('<div>').slick()
+    const slick = $tmp[0].slick.constructor
+    $tmp.slick('unslick')
+    return slick
+  }
 
   if ($.fn.slick) {
-    var Slick = getSlick();
+    const Slick = getSlick()
     if (Slick) {
-      //hook checkResponsive method
-      var checkResponsiveOrig = Slick.prototype.checkResponsive;
+      // hook checkResponsive method
+      const checkResponsiveOrig = Slick.prototype.checkResponsive
       Slick.prototype.checkResponsive = function(initial, forceUpdate) {
-        var _ = this;
+        const _ = this
         if (_.options.autoSlidesToShow && !_.options.infinite && _.options.variableWidth) {
-          var sliderWidth = _.$slider.width();
-          var width = 0, length = _.$slides.length;
-          for (var i = 0; i < length; i++) {
-            width += $(_.$slides[i]).outerWidth();
+          const sliderWidth = _.$slider.width()
+          let width = 0,
+            length = _.$slides.length
+          for (let i = 0; i < length; i++) {
+            width += $(_.$slides[i]).outerWidth()
           }
-          _.averageSlidesWidth = width / length;
-          _.options.slidesToShow = Math.floor(sliderWidth / _.averageSlidesWidth) || 1;
-          //force update arrows
+          _.averageSlidesWidth = width / length
+          _.options.slidesToShow = Math.floor(sliderWidth / _.averageSlidesWidth) || 1
+          // force update arrows
           if (_.lastSlidesToShow !== _.options.slidesToShow) {
-            _.lastSlidesToShow = _.options.slidesToShow;
+            _.lastSlidesToShow = _.options.slidesToShow
             if (initial === true) {
-              _.currentSlide = _.options.initialSlide;
+              _.currentSlide = _.options.initialSlide
             }
-            _.refresh(initial);
+            _.refresh(initial)
           }
         }
-        return checkResponsiveOrig.apply(this, arguments);
-      };
-      //hook getLeft method
-      var getLeftOrig = Slick.prototype.getLeft;
+        return checkResponsiveOrig.apply(this, arguments)
+      }
+      // hook getLeft method
+      const getLeftOrig = Slick.prototype.getLeft
       Slick.prototype.getLeft = function(slideIndex) {
-        var _ = this;
+        const _ = this
         if (_.options.autoSlidesToShow && !_.options.infinite && _.options.variableWidth) {
-          var targetSlide = _.$slideTrack.children('.slick-slide').eq(slideIndex);
+          const targetSlide = _.$slideTrack.children('.slick-slide').eq(slideIndex)
           if (targetSlide[0]) {
-            var diff = 0;
+            let diff = 0
             if (slideIndex) {
-              var sliderWidth = _.$slider.width();
-              var otherSlidesWidth = (_.slideCount - slideIndex) * _.averageSlidesWidth;
+              const sliderWidth = _.$slider.width()
+              const otherSlidesWidth = (_.slideCount - slideIndex) * _.averageSlidesWidth
               if (otherSlidesWidth < sliderWidth) {
-                diff = sliderWidth - otherSlidesWidth;
+                diff = sliderWidth - otherSlidesWidth
               }
             }
-            return (targetSlide[0].offsetLeft - diff) * -1;
+            return (targetSlide[0].offsetLeft - diff) * -1
           }
-          return  0;
+          return 0
         }
-        return getLeftOrig.apply(this, arguments);
-      };
+        return getLeftOrig.apply(this, arguments)
+      }
     }
   }
-})(jQuery);
+})(jQuery)
 
 const Process = {
-	init : function(){
-		
-		$('.slickjs').each(function() {
-		  $(this).slick({
-				  dots: true,
-				  infinite: true,
-				  speed: 500,
-				  fade: true,
-				  cssEase: 'linear',
-				  rows: 0,
-				  arrows: false,
-				  slidesToShow: 1,
-				  slidesToScroll: 1,
-				  appendDots: $(this).closest('.process').find('.dots-container')
-			});
-		});
-			$('.slickjs-nav').slick({
-			  slidesToShow: 1,
-			  slidesToScroll: 1,
-			  asNavFor: '.slickjs',
-			  dots: false,
-			  focusOnSelect: true,
-			  fade: true,
-			  rows: 0,
-			  arrows: false,
-//			  prevArrow: 'arrow-left',
-//			  nextArrow: 'arrow-right'
-			});
-			
-		  $('.slickjs-2').slick({
-				  dots: false,
-				  infinite: true,
-				  speed: 500,
-				  fade: true,
-				  cssEase: 'linear',
-				  rows: 0,
-				  arrows: false,
-				  slidesToShow: 1,
-				  slidesToScroll: 1,
-			});
-			
-		  $('.slickjs-3').slick({
-				  dots: false,
-				  infinite: true,
-				  speed: 500,
-				  fade: true,
-				  cssEase: 'linear',
-				  rows: 0,
-				  arrows: true,
-				  slidesToShow: 1,
-				  slidesToScroll: 1,
-			});
-			
-		  $('.slickjs-3-nav').slick({
-				  dots: false,
-				  infinite: false,
-//				  centerMode: true,
-				  focusOnSelect: true,
-				  speed: 500,
-				  cssEase: 'linear',
-				  asNavFor: '.slickjs-3',
-				  rows: 0,
-				  arrows: true,
-//				  autoSlidesToShow: true,
-//				  variableWidth: true,
-			      slidesToShow: 4,
-				  responsive: [
-				    {
-				      breakpoint: 768,
-				      settings: {
-				        centerMode: false
-				      }
-				    }
-				   ]
-			});
-			
-			$('.slickjs-nav-2').slick({
-			  slidesToShow: 1,
-			  slidesToScroll: 1,
-			  asNavFor: '.slickjs-2',
-			  dots: true,
-			  focusOnSelect: true,
-			  fade: true,
-			  rows: 0,
-			  arrows: true,
-//			  prevArrow: 'arrow-left',
-//			  nextArrow: 'arrow-right'
-			});
-			
-		  $('.carousel').slick({
-				  dots: false,
-				  infinite: true,
-				  speed: 500,
-				  cssEase: 'linear',
-				  rows: 0,
-				  arrows: true,
-				  slidesToShow: 6,
-				  slidesToScroll: 1,
-				  responsive: [
-				    {
-				      breakpoint: 1024,
-				      settings: {
-				        slidesToShow: 3,
-				        slidesToScroll: 3,
-				        infinite: true,
-				        dots: false
-				      }
-				    },
-				    {
-				      breakpoint: 600,
-				      settings: {
-				        slidesToShow: 2,
-				        slidesToScroll: 2
-				      }
-				    },
-				    {
-				      breakpoint: 480,
-				      settings: {
-				        slidesToShow: 1,
-				        slidesToScroll: 1,
-				        arrows: true,
-				      }
-				    }
-				    // You can unslick at a given breakpoint now by adding:
-				    // settings: "unslick"
-				    // instead of a settings object
-				  ]
-			});
-	}
+  init() {
+    $('.slickjs').each(function() {
+      $(this).slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        rows: 0,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        appendDots: $(this)
+          .closest('.process')
+          .find('.dots-container')
+      })
+    })
+    $('.slickjs-nav').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.slickjs',
+      dots: false,
+      focusOnSelect: true,
+      fade: true,
+      rows: 0,
+      arrows: false
+      //			  prevArrow: 'arrow-left',
+      //			  nextArrow: 'arrow-right'
+    })
+
+    $('.slickjs-2').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      infinite: false,
+      cssEase: 'linear',
+      rows: 0,
+      arrows: false,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    })
+
+    $('.slickjs-3').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      rows: 0,
+      arrows: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    })
+
+    $('.slickjs-3-nav').slick({
+      dots: false,
+      infinite: false,
+      //				  centerMode: true,
+      focusOnSelect: true,
+      speed: 500,
+      cssEase: 'linear',
+      asNavFor: '.slickjs-3',
+      rows: 0,
+      arrows: true,
+      //				  autoSlidesToShow: true,
+      //				  variableWidth: true,
+      slidesToShow: 4,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            centerMode: false
+          }
+        }
+      ]
+    })
+
+    $('.slickjs-nav-2').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.slickjs-2',
+      dots: true,
+      focusOnSelect: true,
+      fade: true,
+      infinite: false,
+      rows: 0,
+      arrows: true
+      //			  prevArrow: 'arrow-left',
+      //			  nextArrow: 'arrow-right'
+    })
+
+    $('.about-carousel').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      cssEase: 'linear',
+      arrows: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            centerMode: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    })
+  }
 }
 
 export default Process
